e404802f71b5741901620425211ef4f9935e4022
mdkgray <mdkulen@gmail.com>
2024-11-29 12:27:40 +1100

    modified loginPage.py

--- ACTIVITY-1/tests/test_login.py
+++ ACTIVITY-1/tests/test_login.py
@@ -1,9 +1,13 @@
 import pytest  
 import unittest
+import logging
 
 from utils.config_reader import ConfigReader
 from pages.loginPage import LoginPage
 from pages.productPage import ProductPage
+
+# Setting up logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 class LoginTests(unittest.TestCase):
@@ -17,19 +21,29 @@ class LoginTests(unittest.TestCase):
     # Test for valid login flow using the standard user account    
     @pytest.mark.login
     def test_valid_login(self):
-        username = self.lp.enterUsername(self.cr.getStandardUser())
-        password = self.lp.enterPassword(self.cr.getPassword())
-        self.lp.login(username, password)
+        logging.info("Starting test: test_valid_login")
+        username = self.cr.getStandardUser()
+        password = self.cr.getPassword()
+        self.lp.enterUsername(username)
+        self.lp.enterPassword(password)
         self.lp.clickLoginButton()
-        # self.pp.assertProductPageTitle()
-        self.lp.logout() # TBD on product page
+        self.pp.validateProductPageTitle()
+        self.pp.logout() 
+        logging.info("Finished test: test_valid_login")
         
     # Test for invalid login flow using the locked out user account    
     @pytest.mark.login
     def test_invalid_login(self):
-        username = self.lp.enterUsername(self.cr.getLockedOutUser())
-        password = self.lp.enterPassword(self.cr.getPassword())
-        self.lp.login(username, password)
+        logging.info("Starting test: test_invalid_login")
+        username = self.cr.getLockedUser()
+        password = self.cr.getPassword()
+        self.lp.enterUsername(username)
+        self.lp.enterPassword(password)
+        self.lp.clickLoginButton()
         errorMessage = self.lp.getLoginErrorMessageText()
         expectedErrorMessage = "Epic sadface: Sorry, this user has been locked out."
         assert errorMessage == expectedErrorMessage
+        logging.info("Finished test: test_invalid_login")
+
+if __name__ == "__main__":
+    unittest.main()
